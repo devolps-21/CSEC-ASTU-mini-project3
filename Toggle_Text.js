6839ee1e6776803a00a5ba870159820d50dcb87b
devolps-21 <survivecan22@gmail.com>
2025-12-19 12:09:35 -0800

    Update Toggle_Text.js

--- Toggle_Text.js
+++ Toggle_Text.js
@@ -1,2 +1,10 @@
 const heading = document.getElementById("head");
 const Toggle = document.getElementById("To");
+Toggle.addEventListener("click", () => {
+     if(heading.textContent==="OFF") {
+         heading.textContent="ON"
+     }
+     else {
+        heading.textContent = "OFF"
+     }
+})
